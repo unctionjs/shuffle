1b041a5763e9de22a3c73b5af8a89481a59d7f0e
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-09 13:19:47 -0700

    Making packages actually run and test and then also flow

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import shuffle from "./"
+import shuffle from "./source.js"
 
 test(({notSame, end}) => {
   notSame(
