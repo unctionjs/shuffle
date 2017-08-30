c06a53bf8875fa1bffac8b3610515f1ea9cf5c65
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-30 09:59:49 -0700

    Another round of upgrades

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 /* eslint-disable immutable/no-let, no-magic-numbers, immutable/no-mutation */
+import key from "@unction/key"
+import exceptKey from "@unction/exceptkey"
+import initial from "@unction/initial"
+import attach from "@unction/attach"
+import keys from "@unction/keys"
+import fresh from "@unction/fresh"
+import length from "@unction/length"
+import isPopulated from "@unction/ispopulated"
+import reduceValues from "@unction/reducevalues"
 
 // While this raises eslint issues, they should be ignored. This is the best possible shuffle implementation I could find.
-export default function shuffle (array: string | ArrayType): string | ArrayType {
-  let counter = array.length
-  const clone = array.slice()
+export default function shuffle (orderedList: string | ArrayType): string | ArrayType {
+  return reduceValues(
+    ({latest, remaining, positions}: {latest: string | ArrayType, remaining: string | ArrayType, positions: Array<number>}): Function =>
+      (value: ValueType): {latest: string | ArrayType, remaining: string | ArrayType} | string | ArrayType => {
+        if (isPopulated(remaining)) {
+          const index = Math.floor(Math.random() * length(positions))
 
-  // While there are elements in the array
-  while (counter > 0) {
-    // Pick a random index
-    const index = Math.floor(Math.random() * counter)
+          return {
+            latest: attach(key(index)(positions))(value)(latest),
+            remaining: initial(remaining),
+            positions: exceptKey(index)(positions),
+          }
+        }
 
-    // Decrease counter by 1
-    counter -= 1
-
-    // And swap the last element with it
-    const temp = clone[counter]
-
-    clone[counter] = clone[index]
-    clone[index] = temp
-  }
-
-  return clone
+        return latest
+      },
+  )(
+    {
+      remaining: orderedList,
+      latest: fresh(orderedList),
+      positions: keys(orderedList),
+    }
+  )(
+    orderedList
+  )
 }
