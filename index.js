3714ad43a0209295497ed47183935b65209f88b5
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,36 +1,15 @@
 /* eslint-disable no-magic-numbers */
-import exceptKey from "@unction/exceptkey"
-import attach from "@unction/attach"
-import fresh from "@unction/fresh"
-import length from "@unction/length"
-import range from "@unction/range"
-import reduceValues from "@unction/reducevalues"
+import exceptKey from "@unction/exceptkey";
+import attach from "@unction/attach";
+import fresh from "@unction/fresh";
+import length from "@unction/length";
+import range from "@unction/range";
+import reduceValues from "@unction/reducevalues";
+const AFTER_PLACE = 1; // While this raises eslint issues, they should be ignored. This is the best possible shuffle implementation I could find.
 
-type TransitionType = [
-  OrderedFunctorType,
-  OrderedFunctorType,
-]
-
-const AFTER_PLACE = 1
-
-// While this raises eslint issues, they should be ignored. This is the best possible shuffle implementation I could find.
-export default function shuffle (orderedList: OrderedFunctorType): OrderedFunctorType {
-  return reduceValues(
-    ([before, after]: TransitionType): UnaryFunctionType =>
-      (value: ValueType): TransitionType | OrderedFunctorType => {
-        const index = range(0)(length(before))
-
-        return [
-          exceptKey(index)(before),
-          attach(index)(value)(after),
-        ]
-      },
-  )(
-    [
-      orderedList,
-      fresh(orderedList),
-    ]
-  )(
-    orderedList
-  )[AFTER_PLACE]
+export default function shuffle(orderedList) {
+  return reduceValues(([before, after]) => value => {
+    const index = range(0)(length(before));
+    return [exceptKey(index)(before), attach(index)(value)(after)];
+  })([orderedList, fresh(orderedList)])(orderedList)[AFTER_PLACE];
 }
