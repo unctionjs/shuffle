29c9575de92a1d5ff4d79c162328b397cdb7bc4c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-13 21:01:45 -0700

    Autolinting

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ import range from "@unction/range";
 import reduceValues from "@unction/reducevalues";
 const AFTER_PLACE = 1; // While this raises eslint issues, they should be ignored. This is the best possible shuffle implementation I could find.
 
-export default function shuffle(orderedList) {
-  return reduceValues(([before, after]) => value => {
+export default function shuffle (orderedList) {
+  return reduceValues(([before, after]) => (value) => {
     const index = range(0)(length(before));
+
+
     return [exceptKey(index)(before), attach(index)(value)(after)];
   })([orderedList, fresh(orderedList)])(orderedList)[AFTER_PLACE];
 }
--- test.js
+++ test.js
@@ -1,117 +1,117 @@
 /* eslint-disable no-magic-numbers */
-import {test} from "tap"
+import {test} from "tap";
 
-import shuffle from "./index"
+import shuffle from "./index";
 
 test(({ok, end}) => {
-  const result = shuffle(["a", "b", "c", "d", "e", "f"])
+  const result = shuffle(["a", "b", "c", "d", "e", "f"]);
 
   ok(
     result.includes("a")
-  )
+  );
 
   ok(
     result.includes("b")
-  )
+  );
 
   ok(
     result.includes("c")
-  )
+  );
 
   ok(
     result.includes("d")
-  )
+  );
 
   ok(
     result.includes("e")
-  )
+  );
 
   ok(
     result.includes("f")
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test(({equal, end}) => {
   equal(
     shuffle(["a", "b", "c", "d", "e", "f"]).length,
     6
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test(({equal, end}) => {
   equal(
     shuffle(["a"]).length,
     1
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test(({ok, end}) => {
   ok(
     shuffle(["a"]).includes("a")
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 
 test(({ok, end}) => {
-  const result = shuffle("abcdef")
+  const result = shuffle("abcdef");
 
   ok(
     result.includes("a")
-  )
+  );
 
   ok(
     result.includes("b")
-  )
+  );
 
   ok(
     result.includes("c")
-  )
+  );
 
   ok(
     result.includes("d")
-  )
+  );
 
   ok(
     result.includes("e")
-  )
+  );
 
   ok(
     result.includes("f")
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test(({equal, end}) => {
   equal(
     shuffle("abcdef").length,
     6
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test(({equal, end}) => {
   equal(
     shuffle("a").length,
     1
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test(({ok, end}) => {
   ok(
     shuffle("a").includes("a")
-  )
+  );
 
-  end()
-})
+  end();
+});
