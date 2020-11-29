fd0dfadf28d01c9cb048a467899611d551488e89
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-29 09:49:51 -0800

    Removing more bad types

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ![Stability][BADGE_STABILITY]
 ![Dependencies][BADGE_DEPENDENCY]
 
-> OrderedArray<A> | Set<A> | RecordType<unknown, A> | string => OrderedArray<A> | Set<A> | RecordType<unknown, A> | string
+> OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string => OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string
 
 Takes an Ordered Functor and returns an Ordered Functor with the same content, but in a random order.
 
--- index.ts
+++ index.ts
@@ -8,8 +8,8 @@ import reduceValues from "@unction/reducevalues";
 
 const AFTER_PLACE = 1;
 
-export default function shuffle<A> (orderedEnumerable: OrderedArray<A> | Set<A> | RecordType<unknown, A> | string) {
-  return reduceValues(([before, after]: [A, A]) => (value: A): OrderedArray<A> | Set<A> | RecordType<unknown, A> | string => {
+export default function shuffle<A> (orderedEnumerable: OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string) {
+  return reduceValues(([before, after]: [A, A]) => (value: A): OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string => {
     const index = range(0)(length(before));
 
     return [exceptKey(index)(before), attach(index)(value)(after)];
